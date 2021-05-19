641b276464430e577b1113e96795f06c3068773f
Jared De La Cruz <jared_de_la_cruz@yahoo.com>
2021-05-19 14:28:40 -0700

    Switch to gut-plugin

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,32 @@
-#[allow(improper_ctypes_definitions)]
-pub mod gut_cert_check {
-    use openssl::ssl::{Ssl, SslContext, SslMethod, SslVerifyMode};
-    use std::net::TcpStream;
+use openssl::ssl::{Ssl, SslContext, SslMethod, SslVerifyMode};
+use std::net::TcpStream;
 
-    #[no_mangle]
-    pub extern "C" fn gut_export_functions() -> String {
-        r#"["cert_check"]"#.to_string()
-    }
+gut_plugin::gut_export!(
+    ["cert_check"],
+    ["Prints the certificate expiration date of the url"]
+);
 
-    #[no_mangle]
-    pub extern "C" fn gut_export_descriptions() -> String {
-        r#"["Prints the certificate expiration date of the url"]"#.to_string()
-    }
+#[no_mangle]
+fn cert_check(ptr: *mut c_char) {
+    let c_string = unsafe { CString::from_raw(ptr) };
+    let str_from_host = c_string.to_str().unwrap();
 
-    #[no_mangle]
-    fn cert_check(url: String) {
-        let context = {
-            let mut ctx = SslContext::builder(SslMethod::tls()).unwrap();
-            ctx.set_verify(SslVerifyMode::empty());
-            ctx.build()
-        };
-        let connector = Ssl::new(&context).unwrap();
-        let stream = TcpStream::connect(format!("{}:443", url)).unwrap();
-        let stream = connector
-            .connect(stream)
-            .map_err(|e| panic!("Failed to connect: {}", e))
-            .unwrap();
-        let cert = stream
-            .ssl()
-            .peer_certificate()
-            .ok_or("Failed to find certificate")
-            .unwrap();
+    let context = {
+        let mut ctx = SslContext::builder(SslMethod::tls()).unwrap();
+        ctx.set_verify(SslVerifyMode::empty());
+        ctx.build()
+    };
+    let connector = Ssl::new(&context).unwrap();
+    let stream = TcpStream::connect(format!("{}:443", str_from_host)).unwrap();
+    let stream = connector
+        .connect(stream)
+        .map_err(|e| panic!("Failed to connect: {}", e))
+        .unwrap();
+    let cert = stream
+        .ssl()
+        .peer_certificate()
+        .ok_or("Failed to find certificate")
+        .unwrap();
 
-        println!("{:?}", cert.not_after());
-    }
+    println!("{:?}", cert.not_after());
 }
